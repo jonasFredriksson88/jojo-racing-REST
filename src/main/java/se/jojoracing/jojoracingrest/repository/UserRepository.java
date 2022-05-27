55a4c1d9b909b1663457e9130c4be712127a521d
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-05-27 09:29:34 -0700

    User service added.

---
+++ src/main/java/se/jojoracing/jojoracingrest/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package se.jojoracing.jojoracingrest.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import se.jojoracing.jojoracingrest.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByName(String name);
+}
