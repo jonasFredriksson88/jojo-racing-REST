20460b73437c608359737a4d6bfa4e708c500899
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-05-27 09:02:21 -0700

    Initial commit

---
+++ src/main/java/se/jojoracing/jojoracingrest/JojoRacingRestApplication.java
@@ -0,0 +1,13 @@
+package se.jojoracing.jojoracingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoRacingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JojoRacingRestApplication.class, args);
+	}
+
+}
